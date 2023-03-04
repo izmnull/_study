bb743876866b49197f99960df320f4fcd527f79f
izmnull <98688029+izmnull@users.noreply.github.com>
2023-03-04 20:08:04 +0900

    FIXED: パスの指定が混沌化しないように設定する
    入出力用のHTML/CSS/JSなどのパスの指定を全てテキストベースで記述すると混沌化するため、node付属のpathモジュールを使用してオブジェクト＝ディレクトリ構成を作成する。
    これにより出力先のパスのディレクトリ断層変更や、ステージング/リリース時の構造違いなどにも柔軟に対応可能になる。

--- Webpack/webpack.config.js
+++ Webpack/webpack.config.js
@@ -1,3 +1,5 @@
+const path = require("path");
+
 /**
  * @param {*} env 環境変数
  * @param {*} argv 引数
@@ -6,6 +8,17 @@ module.exports = (env, argv) => {
   const isDevMode = argv.mode === "development" ? true : false;
   const isStaging = !!env.STAGING;
   const isRelease = !!env.RELEASE;
+  const paths = {};
+
+  paths.dist = {};
+  paths.dist.root = path.resolve(__dirname, "./dist");
+  paths.dist.common = {};
+  paths.dist.common.root = path.resolve(paths.dist.root, "./common");
+  paths.dist.common.js = path.resolve(paths.dist.common.root, "./js");
+
+  paths.src = {};
+  paths.src.root = path.resolve(__dirname, "./src");
+  paths.src.script = path.resolve(paths.src.root, "./script");
 
   return {
     // 構成オプション: "none" | "development" | "production"
@@ -18,13 +31,14 @@ module.exports = (env, argv) => {
 
     // エントリーポイント
     entry: {
-      common: "./src/script/common.js"
+      common: path.resolve(paths.src.script, "./common.js")
     },
 
     // 出力する方法と場所
     // @see https://webpack.js.org/configuration/output/
     output: {
-      filename: "./common/js/common.js"
+      path: paths.dist.common.js,
+      filename: "common.js"
     },
 
     // ファイル監視オプション
