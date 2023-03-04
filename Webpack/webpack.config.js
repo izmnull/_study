30ef866f7c88098dc4cd6bfc931e095588bb3132
izmnull <98688029+izmnull@users.noreply.github.com>
2023-03-04 19:37:28 +0900

    FIXED: 設定ファイルにコメントの追加

--- Webpack/webpack.config.js
+++ Webpack/webpack.config.js
@@ -1,21 +1,41 @@
+/**
+ * @param {*} env 環境変数
+ * @param {*} argv 引数
+ */
 module.exports = (env, argv) => {
-    const isDevMode = argv.mode === "development" ? true : false;
-    const isStaging = !!env.STAGING;
-    const isRelease = !!env.RELEASE;
+  const isDevMode = argv.mode === "development" ? true : false;
+  const isStaging = !!env.STAGING;
+  const isRelease = !!env.RELEASE;
 
-    return {
-        mode: isDevMode ? "development" : "production",
-        target: ["web", "es5"],
-        entry: {
-            common: "./src/script/common.js"
-        },
-        output: {
-            filename: "./common/js/common.js"
-        },
-        watchOptions: {
-            ignored: "**/node_modules"
-        },
-        module: {},
-        plugins: []
-    };
+  return {
+    // 構成オプション: "none" | "development" | "production"
+    mode: isDevMode ? "development" : "production",
+
+    // ターゲット環境
+    // @see https://webpack.js.org/configuration/target/
+    // @example ["node"] | ["webworker"]
+    target: ["web", "es5"],
+
+    // エントリーポイント
+    entry: {
+      common: "./src/script/common.js"
+    },
+
+    // 出力する方法と場所
+    // @see https://webpack.js.org/configuration/output/
+    output: {
+      filename: "./common/js/common.js"
+    },
+
+    // ファイル監視オプション
+    watchOptions: {
+      ignored: "**/node_modules"
+    },
+
+    // モジュール
+    module: {},
+
+    // プラグイン
+    plugins: []
+  };
 };
